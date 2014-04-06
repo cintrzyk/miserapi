1bbbc39299653ef384d3e2ffbcae02474383a99f
Marcin Brzeziński <cintrzyk@gmail.com>
2014-04-06 22:35:08 +0200

    rename secret key

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -1,6 +1,6 @@
 module MailerHelper
   def confirmation_link(token)
-    link = 'http://' + Rails.application.secrets.api_endpoint + user_confirmation_path + "/#{token}"
+    link = 'http://' + Rails.application.secrets.client_domain + user_confirmation_path + "/#{token}"
     link_to 'Confirm your email', link
   end
 end
