800d359bf55d9b8f0aef3fc6f629795b8bccf461
Marcin Brzeziński <cintrzyk@gmail.com>
2014-06-01 23:01:12 +0200

    shared examples for API specs

---
+++ spec/support/shared/api_shared_examples.rb
@@ -0,0 +1,37 @@
+shared_examples 'API request' do |status|
+  it "has response status #{status ||= 200}" do
+    expect(response.status).to eq status
+  end
+
+  it 'returns response with JSON Content-Type header' do
+    expect(response.header['Content-Type']).to match /json/
+  end
+end
+
+shared_examples 'API array response' do |keys|
+  it 'returns an array' do
+    expect(json_response.class).to be Array
+  end
+end
+
+shared_examples 'API array response with length' do |length|
+  it "equals #{length}" do
+    expect(json_response.count).to eq length
+  end
+end
+
+shared_examples 'API array response with keys' do |keys|
+  it "has keys: #{keys.join(', ')}" do
+    json_response.each { |h| expect_same_keys h, keys }
+  end
+end
+
+shared_examples 'API response with keys' do |keys|
+  it "has keys: #{keys.join(', ')}" do
+    expect_same_keys json_response, keys
+  end
+end
+
+def expect_same_keys hash, keys
+  expect(hash.keys - keys).to be_empty
+end
