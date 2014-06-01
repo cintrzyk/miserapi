7d9bd50a8e8785b00a1ff6989150cfedc869c795
Marcin Brzeziński <cintrzyk@gmail.com>
2014-06-01 23:00:45 +0200

    API request helper methods [test]

--- spec/support/api_controller_support.rb
+++ spec/support/api_controller_support.rb
@@ -2,4 +2,15 @@ module APIControllerSupport
   def json_response
     @json_response ||= JSON.parse response.body
   end
+
+  def obj_response
+    @obj_response ||= Hashie::Mash.new json_response
+  end
+
+  %w(get post put delete).each do |method|
+    define_method "api_#{method}" do |user, action, params = {}, request_headers = {}|
+      request.env['HTTP_AUTHORIZATION'] = "Token miser=#{user.auth_token}"
+      self.send method, action, params, request_headers
+    end
+  end
 end
